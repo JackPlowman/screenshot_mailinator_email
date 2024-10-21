82785d54a4ed50bb6c5774eac5f5c2e4fb5c2c7e
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-10-21 22:20:46 +0100

    Add Unit Test (#19)
    # Pull Request
    
    ## Description
    
    Add a unit test to ensure function works.

---
+++ tests/test_screenshot_mailinator_email.py
@@ -0,0 +1,11 @@
+from ..screenshot_mailinator_email import screenshot_mailinator_email
+from unittest.mock import patch
+from unittest.mock import MagicMock
+
+
+@patch("sys.argv", ["https://mailinator.com"])
+@patch("screenshot_mailinator_email.screenshot_mailinator_email.sync_playwright")
+def test_screenshot_mailinator_email(_sync_playwright: MagicMock) -> None:
+    # Act
+    screenshot_mailinator_email()
+    # Assert
