69796c8e4f1292146bb02d6e72c58650b67bf18a
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-10-21 17:33:39 +0100

    Add EditorConfig

--- githooks/pre-commit-scripts/python.sh
+++ githooks/pre-commit-scripts/python.sh
@@ -3,6 +3,6 @@ set -e +x
 
 # Check if there are any changes in the watched folder
 if git diff --cached --name-only | grep -q "^screenshot_mailinator_email/"; then
-	just install
-	just ruff-fix
+  just install
+  just ruff-fix
 fi
